0b8242f7036a698f0eb22dda8813a8050884ca89
Telson Thomas <51788854+techcuriosity@users.noreply.github.com>
2022-10-17 23:49:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'DevOps Rocks !!'
+def index():
+    return "Hello - Welcome to TechCuriosity - This is a Flask App Demo"
+    
+app.run(host="0.0.0.0", port=5555)
